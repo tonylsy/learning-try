6015724a46c5bed830f4290f01145563e47eac41
lisiyuan <lisiyuan@J04-LISIYUAN002.vdesktop.ot-hs.com>
2018-09-13 10:10:26 +0800

    learn add local jar on project

--- testMethod/src/com/handler/HelloworldHandler.java
+++ testMethod/src/com/handler/HelloworldHandler.java
@@ -3,7 +3,10 @@ package com.handler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.google.gson.JsonObject;
 
 @Controller
 public class HelloworldHandler {
@@ -11,5 +14,13 @@ public class HelloworldHandler {
 	public ModelAndView helloworld() {
 		ModelAndView modelAndView = new ModelAndView("index.jsp");
 		return modelAndView;
+	}
+	
+	@RequestMapping(value="testJson.do",method=RequestMethod.POST)
+	@ResponseBody
+	public JsonObject testJson() {
+		JsonObject jsonObject = new JsonObject();
+		jsonObject.addProperty("result", "yes!");
+		return jsonObject;
 	}
 }
