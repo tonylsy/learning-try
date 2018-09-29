b8c365b7e5e710f3095788719bb0d105cb7ac479
lisiyuan <lisiyuan@J04-LISIYUAN002.vdesktop.ot-hs.com>
2018-09-29 17:09:45 +0800

    randomCode

--- testMethod/src/com/mybatisMe/common/CommonUtil.java
+++ testMethod/src/com/mybatisMe/common/CommonUtil.java
@@ -9,18 +9,18 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class CommonUtil{
+public class CommonUtil {
 	public static SqlSessionFactory getSqlFactory() throws IOException {
 		InputStream inputSream = Resources.getResourceAsStream("mybatis-config.xml");
 		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputSream);
 		return sqlSessionFactory;
 	}
-	
-	public static Object getBean(String beanname){
+
+	public static Object getBean(String beanname) {
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
 		return ctx.getBean(beanname);
 	}
-	
+
 	public static Object getBean(Class clazz) {
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
 		return ctx.getBean(clazz);
---
+++ testMethod/src/com/mybatisMe/common/RandomCodeTest.java
@@ -0,0 +1,35 @@
+package com.mybatisMe.common;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.awt.Font;
+
+import org.junit.jupiter.api.Test;
+
+class RandomCodeTest {
+	RandomCodeOnlyNumber randomCodeOnlyNumber = new RandomCodeOnlyNumber();
+	@Test
+	void testCreateFont() {
+		Font font = randomCodeOnlyNumber.createFont();
+		assertNotNull(font);
+		assertEquals(font.getFamily(),"Times New Roman");
+		assertEquals(font.getSize(),18);
+		
+		randomCodeOnlyNumber.setFontFamily("Serif");
+		Font font2 = randomCodeOnlyNumber.createFont();
+		assertNotNull(font2);
+		assertEquals(font2.getFamily(),"Serif");
+		assertEquals(font2.getSize(),18);
+		
+		
+		randomCodeOnlyNumber = new RandomCodeOnlyNumber();
+		randomCodeOnlyNumber.setFontSize(20);
+		Font font3 = randomCodeOnlyNumber.createFont();
+		assertNotNull(font3);
+		assertEquals(font3.getFamily(),"Times New Roman");
+		assertEquals(font3.getSize(),20);
+		
+	}
+
+}
