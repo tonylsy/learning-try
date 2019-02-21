aea2cd5e04a143ae4f6b37c06c666ab0f6fe2586
Administrator <Administrator@lsy>
2019-02-21 10:53:07 +0800

    remove the useless import

--- testMethod/src/com/mybatisMe/junit/UserDaoImplTest.java
+++ testMethod/src/com/mybatisMe/junit/UserDaoImplTest.java
@@ -13,9 +13,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
 import com.mybatisMe.Dao.UserDao;
 import com.mybatisMe.common.CommonUtil;
 import com.mybatisMe.entities.User;
--- testMethod/src/com/mybatisMe/junit/UserSeriveImplTest.java
+++ testMethod/src/com/mybatisMe/junit/UserSeriveImplTest.java
@@ -7,8 +7,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.mybatisMe.common.CommonUtil;
 import com.mybatisMe.entities.User;
