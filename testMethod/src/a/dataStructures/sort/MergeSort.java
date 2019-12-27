2719f7f518d439d5af6fecc290c078f197b32fe9
siri <2923372835@qq.com>
2019-12-27 11:34:40 +0800

    finish MergeSort

---
+++ testMethod/src/a/dataStructures/sort/MergeSort.java
@@ -0,0 +1,53 @@
+package a.dataStructures.sort;
+
+public class MergeSort {
+    public static void sort(Comparable[] arr) {
+        sort(arr, 0, arr.length - 1);
+    }
+
+    public static void sort(Comparable[] arr, int lo, int hi) {
+        if (hi <= lo) {
+            return;
+        }
+        //the merge gap is basic on mid
+        int mid = (lo + hi) / 2;
+        sort(arr, lo, mid);
+        sort(arr, mid + 1, hi);
+        merge(arr, lo, mid, hi);
+    }
+
+    /**
+     * merge two subarray from index of lo and hi in arr
+     *
+     * @param arr original array
+     * @param lo  the lower index
+     * @param mid the middle index
+     * @param hi  the highest index
+     */
+    public static void merge(Comparable[] arr, int lo, int mid, int hi) {
+        if (lo > hi || lo > mid || mid > hi || arr.length == 0) {
+            return;
+        }
+        int i = lo;
+        int j = mid + 1;
+        Comparable[] aux = arr.clone();
+        for (int k = i; k <= hi; k++) {
+            //array between lo and mid has run out
+            if (i > mid) {
+                arr[k] = aux[j];
+                j++;
+            } else if (j > hi) {
+                //array between lo and mid has run out
+                arr[k] = aux[i];
+                i++;
+            } else if (SortUtils.isLess(aux[i], aux[j]) || aux[i].equals(aux[j])) {
+                // array between lo and mid and between mid and hi still need to merge
+                arr[k] = aux[i];
+                i++;
+            } else {
+                arr[k] = aux[j];
+                j++;
+            }
+        }
+    }
+}
--- testMethod/src/a/dataStructures/sort/TestSort.java
+++ testMethod/src/a/dataStructures/sort/TestSort.java
@@ -1,12 +1,15 @@
 package a.dataStructures.sort;
 
 public class TestSort {
-    public static Comparable[] OLD = {0, 10, 15, 2, 4, 0, 4, 8, 5, 9, 15, 0, 16, 15};
-    public static final Comparable[] RESULT = {0, 0, 0, 2, 4, 4, 5, 8, 9, 10, 15, 15, 15, 16};
+    public static Comparable[] OLD = {1, 10, 15, 2, 4, 0, 4, 8, 5, 9, 15, 0, 16, 15};
+    public static Comparable[] SP = {8, 9, 1, 7, 2, 3, 5, 4, 6, 0};
+    public static final Comparable[] RESULT = {0, 0, 1, 2, 4, 4, 5, 8, 9, 10, 15, 15, 15, 16};
 
     public static void main(String[] args) {
         //SelectionSort.sort(OLD);
-        InsertionSort.sort(OLD);
+        //InsertionSort.sort(OLD);
+        //ShellSort.sort(OLD);
+        MergeSort.sort(OLD);
         if (compareArray(OLD, RESULT)) {
             System.out.println("SORT SUCCESS!");
         } else {
