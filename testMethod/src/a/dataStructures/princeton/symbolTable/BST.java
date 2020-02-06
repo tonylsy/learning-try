34572c9bc56ccb63674a317ae4e379733fc774c8
lsy <2923372835@qq.com>
2020-02-06 21:47:38 +0800

    finish binary search tree delete

--- testMethod/src/a/dataStructures/princeton/symbolTable/BST.java
+++ testMethod/src/a/dataStructures/princeton/symbolTable/BST.java
@@ -16,8 +16,8 @@ public class BST<Key extends Comparable<Key>, Value> {
         root = put(root, key, value);
     }
 
-    private Node put(Node<Key, Value> node, Key key, Value value) {
-        if (node == null) return new Node(key, value);
+    private Node<Key, Value> put(Node<Key, Value> node, Key key, Value value) {
+        if (node == null) return new Node<>(key, value);
         int compare = key.compareTo(node.key);
         if (compare == 0) {
             node.value = value;
@@ -44,8 +44,64 @@ public class BST<Key extends Comparable<Key>, Value> {
         return null;
     }
 
+    public void deleteMin() {
+        root = deleteMin(root);
+    }
 
+    private Node<Key, Value> deleteMin(Node<Key, Value> node) {
+        if (node.left == null) return node.right;
+        node.left = deleteMin(node.left);
+        node.count = size(node.left) + size(node.right) + 1;
+        return node;
+    }
 
+    public void delete(Key key) {
+        root = delete(root, key);
+    }
+
+    private Node<Key, Value> delete(Node<Key, Value> node, Key key) {
+        if (node == null) return null;
+        int compare = key.compareTo(node.key);
+        if (compare > 0) {
+            node.right = delete(node.right, key);
+        } else if(compare < 0){
+            node.left = delete(node.left, key);
+        }else{
+           if(node.left == null) return node.right;
+           if(node.right == null) return node.left;
+           Node<Key,Value> temp = node;
+           node = min(node.right);
+           node.left = temp.left;
+           node.right = deleteMin(temp.right);
+        }
+        node.count = size(node.left) + size(node.right) + 1;
+        return node;
+    }
+
+    public Key min(){
+        return min(root).key;
+    }
+    private Node<Key,Value> min(Node<Key,Value> node){
+        Node<Key,Value> current = node;
+        while(current != null){
+            Node<Key,Value> left = current.left;
+            if(left ==null){
+                return current;
+            }else{
+                current = left;
+            }
+        }
+        return null;
+    }
+
+    private int size(Node<Key, Value> node) {
+        if (node == null) return 0;
+        return node.count;
+    }
+
+    public int size() {
+        return root.count;
+    }
 
     public Iterable<Key> iterator() {
         return null;
