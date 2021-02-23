9886b9315a65d4c49f7d500fc80ea4256f9e0491
tony <lisiyuan_code@foxmail.com>
2021-02-23 17:36:18 +0800

    add leetcode easy 1

---
+++ testMethod/src/main/java/a/leetcode/MoveZeros.java
@@ -0,0 +1,28 @@
+package a.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * @author Siyuan Li
+ * @version 1.0.0
+ **/
+public class MoveZeros {
+    public static void main(String[] args) {
+        int[] a1 = new int[]{0,1,0,3,12};
+        moveZeroes(a1);
+        System.out.println(Arrays.equals(a1,new int[]{1,3,12,0,0}));//true
+    }
+
+    public static void moveZeroes(int[] nums) {
+        int noZeroIndex=0;
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i]!=0){
+                nums[noZeroIndex] = nums[i];
+                noZeroIndex++;
+            }
+        }
+        for (int i = noZeroIndex; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+}
